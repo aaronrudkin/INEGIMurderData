1188e0163ab293253cdc46a8686b6c285634fec8
Aaron Rudkin <junk_signups@rudkin.ca>
2017-04-24 22:46:08 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# INEGIMurderData
-Monthly city-level murder data from INEGI (Mexico)
+# INEGI Murder Data
+
+Monthly city-level murder data from [p/proyectos/bd/continuas/mortalidad/defuncioneshom.asp?s=est](INEGI (Mexico)). 
+
+## Contents
+
+Data is available in `out.csv`; the same data with non-roman characters removed from city and state names are in `out-fixed.csv`.
+
+The annual CSVs and Python code for the crosswalk are in `raw_source/`
+
+## Sample data
+
+| Year | Month | ID | State | City | Murders |
+| ---- | ----- | -- | ----- | ---- | ------- |
+| 1990 | 1 | 01 001 | Aguascalientes | Aguascalientes | 1 |
+| 1990 | 2 | 01 001 | Aguascalientes | Aguascalientes | 1 |
+| 1990 | 3 | 01 001 | Aguascalientes | Aguascalientes | 2 |
+| 1990 | 4 | 01 001 | Aguascalientes | Aguascalientes | 5 |
+| 1990 | 5 | 01 001 | Aguascalientes | Aguascalientes | 3 |
+| 1990 | 6 | 01 001 | Aguascalientes | Aguascalientes | 3 |
+| 1990 | 7 | 01 001 | Aguascalientes | Aguascalientes | 7 |
+| 1990 | 8 | 01 001 | Aguascalientes | Aguascalientes | 1 |
+| 1990 | 9 | 01 001 | Aguascalientes | Aguascalientes | 1 |
+| 1990 | 10 | 01 001 | Aguascalientes | Aguascalientes | 2 |
+| 1990 | 11 | 01 001 | Aguascalientes | Aguascalientes | 5 |
+| 1990 | 12 | 01 001 | Aguascalientes | Aguascalientes | 2 |
+
---
+++ raw_source/in.py
@@ -0,0 +1,58 @@
+""" Crosswalks INEGI murder data into year-month-city row form. """
+
+import csv
+import unidecode
+import unicodedata
+
+def transcode(fix_text):
+	""" Runs transcode. """
+	results = [["Year", "Month", "ID", "State", "City", "Murders"]]
+	for y in xrange(1990, 2016):
+		filename = str(y)+".csv"
+		with open(filename, "rb") as f:
+			read = csv.reader(f, delimiter=",", quotechar='"')
+			for r in read:
+				if len(r) < 10:
+					continue
+
+				if not len(r[0].strip()):
+					continue
+
+				if r[0].strip().startswith("=CONCAT"):
+					if fix_text:
+						state = unidecode.unidecode(r[1].strip().decode("iso-8859-1"))
+					else:
+						state = r[1].strip()
+					continue
+
+				for m in xrange(3, 15):
+					murders = 0
+					if r[m]:
+						murders = r[m]
+
+					if fix_text:
+						city = unidecode.unidecode(r[1].strip().decode("iso-8859-1"))
+					else:
+						city = r[1].strip()
+
+					new_line = [y, m-2, r[0].strip(), state, city, murders]
+					results.append(new_line)
+
+	if fix_text:
+		out_file_name = "out-fixed.csv"
+	else:
+		out_file_name = "out.csv"
+
+	with open(out_file_name, "wb") as f:
+		out = csv.writer(f, delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL)
+		for result in results:
+			try:
+				out.writerow(result)
+			except:
+				print result
+
+	print len(results)
+
+if __name__ == "__main__":
+	transcode(0)
+	transcode(1)
